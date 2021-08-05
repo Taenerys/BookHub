8808058249571d116b035aa2792e155436fbc17d
Lint Action <lint-action@samuelmeuli.com>
2021-08-05 17:54:18 +0000

    Fix code style issues with Black

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,10 @@
 import os
 from flask import Flask, request, render_template
 from dotenv import load_dotenv
+
 # from . import db
 from werkzeug.security import check_password_hash, generate_password_hash
+
 # from app.db import get_db
 
 from flask_sqlalchemy import SQLAlchemy
@@ -13,8 +15,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template('index.html',url=os.getenv("URL"))
+    return render_template("index.html", url=os.getenv("URL"))
+
 
 @app.route("/new")
-def new(): 
-    return render_template('new.html',url=os.getenv("URL"))
+def new():
+    return render_template("new.html", url=os.getenv("URL"))
