80347114f66ca9944746ecfe97e78a2e0e5768e3
EmilyXinyi <xinyi_0302@yahoo.com>
2021-08-05 13:50:39 -0400

    added endpoints in init.py and linked it to two temporary html pages

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,20 @@
-from flask import Flask
+import os
+from flask import Flask, request, render_template
+from dotenv import load_dotenv
+# from . import db
+from werkzeug.security import check_password_hash, generate_password_hash
+# from app.db import get_db
+
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return "<h1>Book Hub</h1>"
+    return render_template('index.html',url=os.getenv("URL"))
+
+@app.route("/new")
+def new(): 
+    return render_template('new.html',url=os.getenv("URL"))
