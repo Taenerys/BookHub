94d50c5c14ed4d1572e71465416867e3a42a0cc6
Taenerys <thuanh99@student.fdu.edu>
2021-08-06 14:40:41 -0700

    get the create thoughts form working

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,8 @@
 import os
-from flask import Flask, render_template
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, send_file
 from . import db
 from app.db import get_db
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 app.config["DATABASE"] = os.path.join(os.getcwd(), "flask.sqlite")
@@ -15,18 +15,29 @@ def home():
 
 
 @app.route("/create")
-@app.route("/create", methods=("GET", "POST"))
 def create_thoughts():
+    return render_template("create-thought.html")
+
+@app.route("/upload", methods=("GET", "POST"))
+def upload():
     if request.method == "POST":
+        # get the user input from Create form
         book_title = request.form.get("book_name")
         book_author = request.form.get("book_author")
         date_added = request.form.get("date_added")
-        book_image = request.form.get("book_image")
+        book_image = request.files.get("book_image")
         book_notes = request.form.get("book_notes")
+
+        # manipulate book image data
+        # file name of the image
+        book_image_name = secure_filename(book_image.filename)
+        # type of the image (e.g: jpeg, png)
+        book_image_mimetype = book_image.mimetype
 
         db = get_db()
         error = None
 
+        # error checking
         if not book_title:
             error = "Book title is required"
         elif not book_author:
@@ -35,17 +46,21 @@ def create_thoughts():
             error = "Date added is required"
         elif not book_image:
             error = "Book image is required"
+        elif not book_image_name:
+            error = "Bad upload - filename"
+        elif not book_image_mimetype:
+            error = "Bad upload - mimetype"
         elif not book_notes:
             error = "Book notes is required"
 
+        # if there is no error, add to database and return successful code
         if error is None:
             db.execute(
-                "INSERT INTO books (title, author, date_added, image, notes) VALUES (?, ?, ?, ?, ?)",
-                (book_title, book_author, date_added, book_image, book_notes),
+                "INSERT INTO books (title, author, image, date_added, image_name, image_mimetype, notes) VALUES (?, ?, ?, ?, ?, ?, ?)",
+                (book_title, book_author, date_added, book_image.read(), book_image_name, book_image_mimetype, book_notes),
             )
             db.commit()
             return f"Book {book_title} added successfully"
         else:
             return error, 418
-
-    return render_template("create-thought.html")
+    return "Submit action done!", 200
