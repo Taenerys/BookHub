e20dbf77cbf8136eca3f073c4785889ea2962208
Taenerys <thuanh99@student.fdu.edu>
2021-08-20 06:40:37 -0700

    add functional and unit tests

---
+++ tests/conftest.py
@@ -0,0 +1,17 @@
+import os.path
+import sys
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+import pytest
+
+from app import create_app
+
+flask_app = create_app()
+
+@pytest.fixture
+def app():
+    yield flask_app
+
+@pytest.fixture
+def client(app):
+    return app.test_client()
---
+++ tests/functional/test_book.py
@@ -0,0 +1,30 @@
+from app import create_app
+
+def test_home_page():
+    """
+    GIVEN a Flask application configured for testing
+    WHEN thhe '/' page is requested (GET)
+    THEN check that the response is valid
+    """
+    flask_app = create_app()
+
+    # create a test client using the Flask app configured for testig
+    with flask_app.test_client() as test_client:
+        response = test_client.get('/auth/login?next=%2F')
+        assert response.status_code == 200
+        # assert b"Welcome to Book Hub!" in response.data
+        # assert b"Needs an account?" in response.data
+        # assert b"Existing user?" in response.data
+
+def test_home_page_post():
+    """
+    GIVEN a Flask application configured for testing
+    WHEN the '/' page is is posted to (POST)
+    THEN check that a '405' status code is returned
+    """
+    flask_app = create_app()
+
+    #Create a test client using the Flask application configured for testing
+    with flask_app.test_client() as test_client:
+        response = test_client.post('/')
+        assert response.status_code == 405
